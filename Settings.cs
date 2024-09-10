9f3a6761bf8e20a496efc22333fd9b2d07c8f067
Mike <matthews.r.mike@gmail.com>
2024-09-10 19:23:37 +0200

    Add markers settings

--- Settings.cs
+++ Settings.cs
@@ -1,4 +1,6 @@
+using UnityEngine;
 using UnityModManagerNet;
+
 using static UnityModManagerNet.UnityModManager;
 
 namespace ModBase
@@ -6,11 +8,17 @@ namespace ModBase
     public class Settings : ModSettings, IDrawable
     {
         // [Draw(DrawType.)]
+        
+        [Header("Debug")]
+        [Draw(DrawType.Toggle)]
+        public bool showMarkers;
 
         public override void Save(ModEntry modEntry) => Save(this, modEntry);
 
         public void OnChange()
         {
+            Main.SetMarkers(showMarkers);
+
             //
         }
     }
